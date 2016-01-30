0f0ff6efb4fc78d7b55963237cc9ba63d1da0c2e
FumiakiSuzuki <truth.of.faith@gmail.com>
2016-01-30 20:41:28 +0900

    オープニング画面実装
    　・背景画像を仮画像に差し替え
    　・表示文字：アニメーション追加、表示時間の変更

---
+++ KitKat/Assets/Scripts/Opening/OpeningText.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningText : MonoBehaviour {
+	
+    [SerializeField]
+    private float velocityY;// 文字の移動速度
+
+    [SerializeField]
+    private int pauseTimeFirstTime;// 最初の文字を止めておく時間
+
+    private int pauseTime = 0;
+
+    void Update () {
+        if( pauseTime < pauseTimeFirstTime)
+        {
+            pauseTime++;
+            return;
+        }
+        Vector3 pos = transform.position;
+        pos.y += velocityY;
+        transform.position = pos;
+
+    }
+}
