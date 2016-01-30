4fb1e5303c18a979f74fcf97bb7615604c9c064b
IItoYukn <ippeito6665@gmail.com>
2016-01-30 13:08:24 +0900

    Enemyタグを追加しました。敵1の動きを追加しました。ノックバックを追加しました

---
+++ KitKat/Assets/Enemy/Enemy1/Enemy1.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : EnemyState {
+	//赤鬼のスクリプト
+	int initTime;//ノックバック時間の初期値	
+	// Use this for initialization
+	void Start () {
+		PanelZSize = 5;
+		getPosition();//初期座標取得
+		speed = 3;//速さ設定
+		HP=2;//HP設定
+		NockBack = false;
+		Escape = false;
+		AtackFlag = false;
+		NockBackTime = 60;//ノックバック時間の設定
+		initTime=NockBackTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//debug
+		if(Input.GetKeyDown(KeyCode.A)){
+			//Bend ();
+			//Escape = true;
+		}
+	}
+	void FixedUpdate(){
+		getPosition ();//座標更新
+		line = getLineNumber (Position);//行番更新
+		if(!NockBack&&!Escape){//仰け反ってない状態かつやられていない状態
+		Move ();
+		}
+			//仰け反り
+		if(NockBack&&!Escape){
+			BendTimeReduce ();
+		}
+
+
+		
+		if(Escape){
+			EscapeRun ();
+		}
+		//
+	}
+	void Move(){//キャラの動き　真っ直ぐ
+		transform.position = Position + Vector3.right * Time.deltaTime*speed;
+	}
+	void Bend(){//仰け反り
+		
+		NockBack = true;
+
+	}
+	void BendTimeReduce(){//
+		--NockBackTime;
+		if(NockBackTime<0){
+			NockBackTime = initTime;//初期値設定
+			NockBack = false;
+
+		}
+	}
+	void ReduceHP(){//HPを1減らす
+		--HP;
+	}
+	void EscapeRun(){//やられて逃げる時の動き
+		//とりあえず来たレーンを走って戻る
+		speed = 10;//走るため速く
+		transform.position = Position + Vector3.left * Time.deltaTime*speed;
+	}
+	void Atack(){
+
+	}
+}
--- KitKat/Assets/Enemy/EnemyState.cs
+++ KitKat/Assets/Enemy/EnemyState.cs
@@ -2,34 +2,40 @@
 using System.Collections;
 
 public class EnemyState : MonoBehaviour {
+	public int PanelZSize;//1マスの幅
+	public int PanelXSize;//1マスの長さ
 	public int line;//敵のいる行(ライン)
 	public int column;//敵のいる列
 	protected int HP;//ヒットポイント
-	public Vector3 Position;//現在座標
+	protected int speed;//速さ
+	protected Vector3 Position;//現在座標
+	protected bool NockBack;//ノックバック
+	protected int NockBackTime;//ノックバック時間
+	protected bool Escape;//逃げる(やられた)
+	protected bool AtackFlag;//攻撃するフラグ
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 	void FixedUpdate(){
-		getPosition();//座標更新
-		line = getLineNumber(Position);//行番更新
+		
 	}
-	void getPosition(){
+	public void getPosition(){
 		Position = transform.position ;// 座標取得
 	}
-	int getLineNumber(Vector3 pos){//行番号取得
-		if(pos.z>2.5f&&pos.z<7.5f){
+	public int getLineNumber(Vector3 pos){//行番号取得
+		if(pos.z>PanelZSize/2&&pos.z<PanelZSize+PanelZSize/2){
 			return 1;
 		}
-		if(pos.z>-2.5f&&pos.z<2.5f){
+		if(pos.z>-PanelZSize/2&&pos.z<PanelZSize/2){
 			return 2;
 		}
-		if(pos.z<-2.5f&&pos.z>-7.5f){
+		if(pos.z<-PanelZSize/2&&pos.z>-PanelZSize-PanelZSize/2){
 			return 3;
 		}
 		return 0;
