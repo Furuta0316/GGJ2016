acc916af45a95b0e4ba3e16f6fcd146f9995b2fb
Ochinosin <m0113099c0@edu.teu.ac.jp>
2016-01-30 20:27:55 +0900

    豆なげる時放物線になげれるようにした

--- KitKat/Assets/Scripts/Mame/Mame_oti.cs
+++ KitKat/Assets/Scripts/Mame/Mame_oti.cs
@@ -7,12 +7,20 @@ public class Mame_oti : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody> ().velocity = transform.right.normalized * speed;
+		GetComponent<Rigidbody> ().AddForce (-500.0f, 250.0f, 0.0f, ForceMode.Force);
+		//GetComponent<Rigidbody> ().velocity = transform.right.normalized * speed;
 		Destroy (this.gameObject, time);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnCollisionEnter(Collision c){
+		if (c.gameObject.tag == "Enemy") {
+			
+			GameObject.Find ("Status").GetComponent<Score> ().kazu += 100;
+		}
 	}
 }
